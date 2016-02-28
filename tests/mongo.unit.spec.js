e24c8009d839094e078f9d6cf385cc4bb4523a62
Oleg verhovsky <verchol@gmail.com>
2016-02-28 14:17:36 +0200

    Fix

---
+++ tests/mongo.unit.spec.js
@@ -0,0 +1,26 @@
+var mognoose = require('mongoose');
+describe('test mongo', function(){
+  var DB = process.env.DB  || 'mongodb://mongo/local';
+
+  it('connect', function(done){
+    var mongoose = require('mongoose');
+
+    mongoose.connect(DB);
+        // CONNECTION EVENTS
+    // When successfully connected
+    mongoose.connection.on('connected', function () {
+      console.log('Mongoose default connection open to ' + DB);
+      done();
+    });
+
+    // If the connection throws an error
+    mongoose.connection.on('error',function (err) {
+      console.log('Mongoose default connection error: ' + err);
+      done(err);
+    });
+
+  });
+
+
+
+});
