53b6db2cbc7523019a935fff76fbf4b14aa9d94f
Oleg verhovsky <verchol@gmail.com>
2016-07-07 16:05:06 +0300

    fix

--- scripts/slack.sh
+++ scripts/slack.sh
@@ -1,1 +1,3 @@
+
+curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"-----------------\"}"  https://hooks.slack.com/services/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
 curl -X POST -H 'Content-type: application/json' --data "{\"text\": \"$MESSAGE\"}"  https://hooks.slack.com/services/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
